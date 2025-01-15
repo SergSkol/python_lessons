f9867aee0a05d13a9df36b5789b3af467b4a5701
Sergiy Shkolnik <sps676@gmail.com>
2025-01-15 10:04:20 +0100

    add test case

--- lesson_1/test_file.py
+++ lesson_1/test_file.py
@@ -1,5 +1,13 @@
 import pandas
 
-d= {'a': [1,2,3]}
+d= {'a': [1,2,3], 'b': [4, 5, 6]}
+print('create dict...')
+print(pandas.DataFrame(d))
 
+d['c'] = [3,2,1]
+print('add key value pair...')
+print(pandas.DataFrame(d))
+
+del d['a']
+print('delete key value pair...')
 print(pandas.DataFrame(d))
